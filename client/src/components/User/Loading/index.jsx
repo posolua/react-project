df606f9582d3d630ec65a9587298fa90921bd96f
Serhii Poslovskyi <s.poslovskiy@gmail.com>
2019-02-10 22:04:51 +0200

    update commit

--- client/src/components/User/Loading/index.jsx
+++ client/src/components/User/Loading/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import s from './Loading.module.css';
-const Loading = () => {
-  return (
-    <div className={s.loading}>
-      <p>Loading....</p>
-    </div>
-  );
-};
+
+const Loading = () => (
+  <div className={s.loading}>
+    <p>Loading....</p>
+  </div>
+);
 
 export default Loading;
